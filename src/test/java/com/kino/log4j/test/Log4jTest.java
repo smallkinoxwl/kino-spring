3dc641539035b425d6a1d6ced3fac75eecdbe915
xiaoxiaofeng <xiaoxiaofeng@bubugao.com>
2016-04-19 14:29:02 +0800

    框架

---
+++ src/test/java/com/kino/log4j/test/Log4jTest.java
@@ -0,0 +1,15 @@
+package com.kino.log4j.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class Log4jTest {
+	
+	private static final Logger logger = LogManager.getLogger(Log4jTest.class);
+	
+	@Test
+	public void testHelloWorld(){
+		logger.info("HelloWorld!");
+	}
+}
