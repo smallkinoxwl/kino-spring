4220826f9c84a4120ec6521578d2d328e229b839
user <user@HQWJ-17>
2016-04-24 21:31:04 +0800

    sql

--- src/main/java/kino/spring/test/mvc/service/WorkmanService.java
+++ src/main/java/kino/spring/test/mvc/service/WorkmanService.java
@@ -1,11 +1,14 @@
 package kino.spring.test.mvc.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 
 @Service
 public interface WorkmanService {
 
-	public int getWorkmanInfo();
+	public List<Map<String, Object>> getWorkmanInfo(int i,String s);
 
 }
--- src/main/java/kino/spring/test/mvc/service/impl/WorkmanServiceImpl.java
+++ src/main/java/kino/spring/test/mvc/service/impl/WorkmanServiceImpl.java
@@ -1,22 +1,25 @@
 package kino.spring.test.mvc.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import kino.spring.test.dao.WorkmanMapper;
+import kino.spring.test.mvc.service.WorkmanService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+
 @Service
-public class WorkmanServiceImpl {
+public class WorkmanServiceImpl implements WorkmanService{
 
 	@Autowired
 	private WorkmanMapper workmanMapper;
 
-	public int getWorkmanInfo() {
+	public List<Map<String, Object>> getWorkmanInfo(int i,String s) {
 		// TODO Auto-generated method stub
 		List<Map<String, Object>> workmanInfo = workmanMapper.getWorkmanInfo();
 		
@@ -29,7 +32,18 @@ public class WorkmanServiceImpl {
 		}
 		
 		List<Map<String, Object>> workmanImgs = workmanMapper.getWorkmanImgs(phoneList);
-		return 1;
+		for(Map<String, Object> work : workmanInfo){
+			String phone = work.get("phone").toString();
+			List<Object> list = new ArrayList<Object>();
+			
+			for(Map<String, Object> img : workmanImgs){
+				if(phone.equals(img.get("phone"))){
+					list.add(img.get("img"));
+				}
+			}
+			work.put("imgs", list);
+		}
+		return workmanInfo;
 	}
 
 }
